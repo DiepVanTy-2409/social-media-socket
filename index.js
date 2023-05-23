8d1ab28b3eeab6981a4837f841d6a37497f99b78
DiepVanTy <tydv2k3@gmail.com>
2023-05-23 13:56:42 +0700

    add .env file

---
+++ index.js
@@ -0,0 +1,41 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+const io = require('socket.io')(process.env.PORT || 8800, {
+    cors: {
+        // origin: 'http://localhost:5173'
+        origin: process.env.IO_ORIGIN
+    }
+})
+let activeUsers = []
+io.on('connection', (socket) => {
+    // add new user
+    socket.on('new-user-add', (newUserId) => {
+        // if user is not added previously
+        if (!activeUsers.some((user) => user.userId === newUserId)) {
+            activeUsers.push({
+                userId: newUserId,
+                socketId: socket.id
+            })
+        }
+        console.log('connected', activeUsers)
+        io.emit('get-users', activeUsers)
+    })
+
+    // send message
+    socket.on('send-message', (data) => {
+        const { receiverId } = data
+        const user = activeUsers.find(user => user.userId === receiverId)
+        console.log("Sending from socket to: ", receiverId)
+        console.log('DATA:', data)
+        if(user) {
+            io.to(user.socketId).emit('receive-message', data)
+        }
+    })
+
+    socket.on('disconnect', () => {
+        activeUsers = activeUsers.filter(user => user.socketId !== socket.id)
+        console.log('User disconnected', activeUsers);
+        io.emit('get-users', activeUsers)
+    })
+})
+
